379a9116e1c1770003f8faf68c48f47191da78de
ABHISHEK RAJPUT <79101220+thakur06@users.noreply.github.com>
2025-03-01 17:15:12 +0530

    added dependency

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -4,7 +4,7 @@ import { RiSignalWifiOffFill } from "react-icons/ri";
 import { MdOutlineSignalWifiStatusbar4Bar } from "react-icons/md";
 import { PiSpeedometerFill } from "react-icons/pi";
 import { FaSignal } from "react-icons/fa";
-import useNetwork from "./useNetwork";
+import useNetwork from "mynetworkinfo";
 import { TbSignal2G, TbSignal3G, TbSignal4G, TbSignal5G, TbSignalLte } from "react-icons/tb";
 import { GiAerialSignal } from "react-icons/gi";
 
